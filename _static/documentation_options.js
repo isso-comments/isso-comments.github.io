8f5ab1269e26e6d8f2495c2d141fe6cac7c8210c
ix5 <ix5@users.noreply.github.com>
2022-04-29 22:12:05 +0000

    deploy: isso-comments/isso@1b118d341c745e37672e0667f7beba9bbb2d2676

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,14 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT:    '',
-    VERSION:     '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'dirhtml',
     FILE_SUFFIX: '',
-    LINK_SUFFIX: '',
-    HAS_SOURCE:  true,
-    SOURCELINK_SUFFIX:  '.txt',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: false,
-    ENABLE_SEARCH_SHORTCUTS: true,
 };
